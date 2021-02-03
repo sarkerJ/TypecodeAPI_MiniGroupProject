bb4be963598d80495d37f88dbce12ea5a3b2db2e
Oliver Lawrence-Overy <olawrenceovery@hotmail.com>
2021-02-03 12:11:21 +0000

    Added basic project

---
+++ TypecodeAPIService/TypecodeAPIService/TypecodeReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TypecodeAPIService
+{
+    public static class TypecodeReader
+    {
+        public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+    }
+}
